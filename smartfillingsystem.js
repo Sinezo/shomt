27c9dcab28af889c39e9ab82f8611d2d1081f546
Sinezo <lusawanasinethemba@gmail.com>
2023-11-11 21:21:48 +0200

    saving all files

--- smartfillingsystem.js
+++ smartfillingsystem.js
@@ -1,3 +1,14 @@
+//ACCESS KEY
+function ACCESS_KEY(){
+    let value=document.getElementById("access-key").value;
+    if(value=='20234'){
+        document.querySelector('#access-box').style.transform='translate(-100%)';
+        document.querySelector('.project-title').style.display='block';
+        document.querySelector('.wrapper-box').style.display='block';
+
+    }
+}
+
 var position=1;
 var points =0;
 var a=0;
@@ -106,9 +117,9 @@ function CONTINUE(){
                     if(i==8){
                         setTimeout(function(){
                             document.querySelector('#third-theory-questions').style.display='none';
-                            //document.querySelector('#fourth-theory').style.display='block';
+                            document.querySelector('#fourth-theory').style.display='block';
                             document.querySelector('.feedback').innerHTML='';
-                            position=5;
+                            position=7;
                         },2000);
                     }
                 },i*100);
@@ -117,6 +128,49 @@ function CONTINUE(){
         }
         else{
             if(document.querySelector('#A-notverylong').checked || document.querySelector('#B-10seconds').checked || document.querySelector('#D-37seconds').checked){
+                document.querySelector('.feedback').innerHTML='Wrong Answer';
+                document.querySelector('.feedback').style.color='red';
+                /*setTimeout(function(){
+                            document.querySelector('#intro-questions').style.display='none';
+                            document.querySelector('#second-theory').style.display='none';
+                            position=3;
+                        },2000);*/
+            }
+            else{
+                document.querySelector('.feedback').innerHTML='Please Select';
+                document.querySelector('.feedback').style.color='chocolate';
+            }
+        }
+        
+    }
+    else if(position==7){
+        document.querySelector('.mypoints4').innerHTML=points;
+        document.querySelector('#fourth-theory').style.display='none';
+        document.querySelector('#fourth-theory-questions').style.display='block';
+        position=8;
+    }
+    else if(position==8){
+        if(document.querySelector('#D-price').checked){
+            document.querySelector('.feedback').innerHTML='Correct Answer';
+            document.querySelector('.feedback').style.color='green';
+            for(let i=8;i<12;i++){
+                setTimeout(function(){
+                    points=i;
+                    document.querySelector('.mypoints4').innerHTML=points;
+                    if(i==11){
+                        setTimeout(function(){
+                            document.querySelector('#fourth-theory-questions').style.display='none';
+                            document.querySelector('#info-box1').style.display='block';
+                            document.querySelector('.feedback').innerHTML='';
+                            position=9;
+                        },2000);
+                    }
+                },i*100);
+            }
+            
+        }
+        else{
+            if(document.querySelector('#A-price').checked || document.querySelector('#B-price').checked || document.querySelector('#C-price').checked){
                 document.querySelector('.feedback').innerHTML='Wrong Answer';
                 document.querySelector('.feedback').style.color='red';
                 /*setTimeout(function(){
