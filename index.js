69dd8ee77e81d47e164575ca154f7e00112de9b2
Sinezo <lusawanasinethemba@gmail.com>
2023-12-08 16:43:55 +0200

    saving all

--- index.js
+++ index.js
@@ -20,13 +20,20 @@ var state=0;
 function MENU_ICON(){
     if(state==0){
         document.querySelector('.bar1').style.transform='rotate(45deg)';
-        document.querySelector('.bar1').style.margin='8px 0 0 0';
+        document.querySelector('.bar1').style.margin='9px 0 0 0';
         document.querySelector('.bar3').style.transform='rotate(-45deg)';
-        document.querySelector('.bar2').style.margin='-6px 0 0 0';
-        document.querySelector('.wrap-bars').style.border='1px solid';
+        document.querySelector('.bar2').style.margin='-10px 0 0 0';
         document.querySelector('.wrap-bars').style.height='20px';
         document.querySelector('.bar2').style.transform='translate(100%)';
         document.querySelector('.bar2').style.opacity='0';
+        document.querySelector('.left-side-bar').style.transform='translate(0)';
+        document.querySelector('.left-side-bar').style.zIndex='1';
+        document.querySelector('.left-side-bar').style.opacity='1';
+        document.querySelector('.left-side-bar').style.position='fixed';
+        document.querySelector('.left-side-bar').style.width='100%';
+        document.querySelector('.left-side-bar').style.top='0';
+        document.querySelector('.left-side-bar').style.height='100%';
+        
         state=1;
     }
     else{
@@ -39,6 +46,8 @@ function MENU_ICON(){
         document.querySelector('.wrap-bars').style.height='20px';
         document.querySelector('.bar2').style.transform='translate(0)';
         document.querySelector('.bar2').style.opacity='1';
+        document.querySelector('.left-side-bar').style.transform='translate(-100%)';
+        document.querySelector('.left-side-bar').style.opacity='0';
         state=0;
     }
 }
